b7c1f6bf2ab98bef1f467d5dc9c355fe3b9606eb
Wafa Mohessin <wafamohessin@gmail.com>
2022-01-27 23:48:23 -0600

    creating OrderHistory link

--- redux-store/client/src/components/Nav/index.js
+++ redux-store/client/src/components/Nav/index.js
@@ -8,6 +8,25 @@ function Nav() {
         if (Auth.loggedIn()) {
 
     return (
+
+      <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">
+              Order History
+            </Link>
+          </li>
+          <li className="mx-1">
+
+          <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+
+    } else {
+      return (
+
         <ul className="flex-row">
           <li className="mx-1">
             <Link to="/signup">
